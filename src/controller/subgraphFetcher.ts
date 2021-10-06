064d4e95e88b129138ab1cdcfe8cc644bf524237
Hernando Guzmán <hernando.guzman93@gmail.com>
2021-10-06 13:53:35 -0300

    Add functions to fetch active proposals and config params

--- src/controller/subgraphFetcher.ts
+++ src/controller/subgraphFetcher.ts
@@ -1,8 +1,11 @@
+import BigNumber from '../lib/bigNumber';
+
 import { graphqlClient } from '../apollo/client';
 
 import {
   PROPOSAL_TITLE_QUERY,
-  ACTIVE_PROPOSALS_QUERY
+  ACTIVE_PROPOSALS_QUERY,
+  CONVICTION_PARAMS_QUERY
 } from '../apollo/queries';
 
 export const fetchProposalTitle = async (id: number): Promise<string> => {
@@ -22,10 +25,10 @@ export const fetchProposalTitle = async (id: number): Promise<string> => {
 };
 
 interface Proposal {
-  convictionLast: string;
+  convictionLast: BigNumber;
   metadata: string;
   number: string;
-  requestedAmount: string;
+  requestedAmount: BigNumber;
 }
 
 export const fetchActiveProposals = async (): Promise<Proposal[]> => {
@@ -33,7 +36,50 @@ export const fetchActiveProposals = async (): Promise<Proposal[]> => {
     const result = await graphqlClient.query({ query: ACTIVE_PROPOSALS_QUERY });
     if (!result.data || !result.data.proposals.length) return undefined;
 
-    return result.data.proposals;
+    return result.data.proposals.map((proposal) => ({
+      convictionLast: new BigNumber(proposal.convictionLast),
+      metadata: proposal.metadata,
+      number: proposal.number,
+      requestedAmount: new BigNumber(proposal.requestedAmount)
+    }));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+interface ConvictionParams {
+  decay: BigNumber;
+  maxRatio: BigNumber;
+  minThresholdStakePercentage: BigNumber;
+  pctBase: BigNumber;
+  totalStaked: BigNumber;
+  weight: BigNumber;
+}
+
+export const fetchConvictionParams = async (): Promise<ConvictionParams> => {
+  try {
+    const result = await graphqlClient.query({
+      query: CONVICTION_PARAMS_QUERY
+    });
+
+    if (!result.data || !result.data.convictionConfigs.length) return undefined;
+
+    const convictionParams = result.data.convictionConfigs[0];
+
+    return {
+      decay: new BigNumber(convictionParams.decay),
+      maxRatio: new BigNumber(convictionParams.maxRatio).dividedBy(
+        new BigNumber(convictionParams.pctBase)
+      ),
+      minThresholdStakePercentage: new BigNumber(
+        convictionParams.minThresholdStakePercentage
+      ),
+      pctBase: new BigNumber(convictionParams.pctBase),
+      totalStaked: new BigNumber(convictionParams.totalStaked),
+      weight: new BigNumber(convictionParams.weight).dividedBy(
+        new BigNumber(convictionParams.pctBase)
+      )
+    };
   } catch (err) {
     console.log(err);
   }
